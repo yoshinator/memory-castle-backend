425f7426e6a550ff9ce1719ea6f9bca4b4f6549c
Yoan Ante <yoanante@gmail.com>
2019-02-01 16:38:45 -0500

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '192.243.101.148:80'
+    origins 'http://yoanante.com/*'
 
     resource '*',
       headers: :any,
