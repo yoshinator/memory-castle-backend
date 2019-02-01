ecb024418e33f2c8afb61453a9f33c82a0f74f8b
Yoan Ante <yoanante@gmail.com>
2019-02-01 16:29:33 -0500

    cors policy change

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://yoanante.com'
+    origins 'http://yoanante.com:3011'
 
     resource '*',
       headers: :any,
