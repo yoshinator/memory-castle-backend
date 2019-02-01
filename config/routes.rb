bb7250cb19300969ca1f40ebeb79fe2cd4a99d3d
Yoan Ante <yoanante@gmail.com>
2019-02-01 14:38:06 -0500

    testing on dev server

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-  namespace 'memorycastleapi', path: '/' do
+  scope: path: 'memorycastleapi' do
     namespace :api do
       namespace :v1 do
         resources :users
         resources :castles
         resources :memories
+      end
     end
   end
-end
   
 end
