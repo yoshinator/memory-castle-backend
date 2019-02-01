5131c9821997242f180a2ec7cd5c2d567fc5847d
Yoan Ante <yoanante@gmail.com>
2019-02-01 14:30:16 -0500

    changed namespace to work on server with multiple apps

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace 'memorycastle', path: '/' do
     namespace :api do
       namespace :v1 do
         resources :users
+        resources :castles
+        resources :memories
     end
   end
-  
-  namespace :api do
-    namespace :v1 do
-      resources :castles
-    end
-  end
- 
-  namespace :api do
-    namespace :v1 do
-      resources :memories
-    end
-  end
+end
   
 end
